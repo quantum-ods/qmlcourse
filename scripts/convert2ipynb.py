893218dcbeaa94ccbabd60349347de23b3e16aab
Alexey Pronkin <pronkinalexeyvictorovich@gmail.com>
2022-06-22 17:23:14 -0400

    fix convert2ipynb.py; add .dockerignore; add toml2requirements.py

--- scripts/convert2ipynb.py
+++ scripts/convert2ipynb.py
@@ -5,19 +5,31 @@ import yaml
 
 PREFIX = "qmlcourse" + "/"
 
-with open("qmlcourse/_toc.yml") as f:
-    toc = yaml.safe_load(f)
-# make dir for ipynbs
-os.makedirs("notebooks", exist_ok=True)
-# remove index file
-for part in toc[1:]:
-    for chapter in part["chapters"]:
-        call(["poetry", "run", "jupytext", PREFIX + chapter["file"], "--to", "ipynb"])
-        os.makedirs("notebooks/" + "/".join(chapter["file"].split("/")[1:-1]), exist_ok=True)
-        call(
-            [
-                "mv",
-                PREFIX + chapter["file"].split(".")[0] + ".ipynb",
-                "notebooks/" + "/".join(chapter["file"].split("/")[1:]) + ".ipynb",
-            ]
-        )
+
+def convert_md2ipynb(toc):
+    """
+    Convert MyST files from toc to ipython notebooks.
+    toc: YAML list of the dicts with table of contents for jupyter book.
+    """
+    for part in toc[1:]:
+        for chapter in part["chapters"]:
+            call(["poetry", "run", "jupytext", PREFIX + chapter["file"], "--to", "ipynb"])
+            os.makedirs("notebooks/" + "/".join(chapter["file"].split("/")[1:-1]), exist_ok=True)
+            call(
+                [
+                    "mv",
+                    PREFIX + chapter["file"].split(".")[0] + ".ipynb",
+                    "notebooks/" + "/".join(chapter["file"].split("/")[1:]) + ".ipynb",
+                ]
+            )
+
+
+if __name__ == "main":
+    with open("qmlcourse/_toc.yml") as f:
+        toc = yaml.safe_load(f)
+
+    # make dir for ipynbs
+    os.makedirs("notebooks", exist_ok=True)
+
+    # convert lectures
+    convert_md2ipynb(toc)
