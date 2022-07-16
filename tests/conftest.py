f61efa7baba8b5e2afae0c8d6281c05975348418
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2022-07-16 17:41:45 +0300

    test: update

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,45 +2,7 @@ import pytest
 
 
 @pytest.fixture()
-def get_toc_file() -> (str):
-    toc_file = """
-    - file: book/index.md
-
-    # :ru:
-
-    - part: Test
-    chapters:
-        - file: book/test/test.md
-        title: 🟦 Just test
-    """
-
-    return toc_file
-
-
-@pytest.fixture()
-def get_md_file() -> (str):
-    md_file = """
-    # Test
-
-    Some test text
-
-    $$
-    E = mc^2
-    $$
-
-    ```python
-    print(2+2)
-    ```
-
-    """
-
-    return md_file
-
-
-@pytest.fixture()
 def get_ipynb_file() -> (str):
-    ipynb_file = """
-
-    """
+    ipynb_file = '{\n "cells": [\n  {\n   "cell_type": "markdown",\n   "id": "9c9effa9",\n   "metadata": {},\n   "source": [\n    "(test_lec)=\\n",\n    "\\n",\n    "# Test\\n",\n    "\\n",\n    "Some test text\\n",\n    "\\n",\n    "$$\\n",\n    "E = mc^2\\n",\n    "$$"\n   ]\n  },\n  {\n   "cell_type": "code",\n   "execution_count": null,\n   "id": "ca3a3229",\n   "metadata": {},\n   "outputs": [],\n   "source": [\n    "a = 2\\n",\n    "b = 2\\n",\n    "\\n",\n    "a + b"\n   ]\n  },\n  {\n   "cell_type": "markdown",\n   "id": "c4b7952a",\n   "metadata": {},\n   "source": [\n    "Some test text"\n   ]\n  }\n ],\n "metadata": {\n  "kernelspec": {\n   "display_name": "Python 3",\n   "language": "python",\n   "name": "python3"\n  }\n },\n "nbformat": 4,\n "nbformat_minor": 5\n}\n'  # noqa: FS003
 
     return ipynb_file
--- tests/convert_test.py
+++ tests/convert_test.py
@@ -1,19 +1,26 @@
-import os
+from pathlib import Path
 
-import yaml  # type: ignore
+from loguru import logger
 
 from tools.convert import md_to_ipynb
 
 
 # todo: add tests for exception
-def test_md_to_ipynb(get_toc_file: str, get_ipynb_file: str) -> (None):
+def test_md_to_ipynb(get_ipynb_file: str) -> (None):
 
-    # todo: update args
-    md_to_ipynb(yaml.safe_load(get_toc_file))
+    try:
 
-    os.chdir("scripts")
+        md_to_ipynb(dir_2_toc="./tests/data", dir_2_ipynb="./tests", testing=True)
 
-    # Test len ipynb
-    with open(get_ipynb_file, "r") as test_ipynb_file:
-        test_ipynb_readed = test_ipynb_file.read()
-    assert len(test_ipynb_readed) == 685
+        path_to_converted_lecture_to_ipynb = Path("./tests/data/lecture.ipynb")
+
+        # Test len ipynb
+        with open(path_to_converted_lecture_to_ipynb, "r") as test_ipynb_file:
+            converted_lecture_to_ipynb = test_ipynb_file.read()
+
+        assert len(get_ipynb_file) == len(converted_lecture_to_ipynb)
+
+    except FileNotFoundError:
+        logger.error("file not found")
+    finally:
+        path_to_converted_lecture_to_ipynb.unlink()  # pyright: ignore reportUnboundVariable
