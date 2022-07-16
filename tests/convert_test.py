016d6a6c92c30e8d237fdacbf181378e2b109ab1
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2022-07-16 15:43:32 +0300

    test: update tests little bit

--- tests/convert_test.py
+++ tests/convert_test.py
@@ -1,36 +1,19 @@
 import os
-from pathlib import Path
 
 import yaml  # type: ignore
 
 from tools.convert import md_to_ipynb
 
-test_toc = """
-- file: book/index.md
 
-# :ru:
-
-- part: Test
-  chapters:
-    - file: book/test/test.md
-      title: 🟦 Just test
-"""
-
-
-def test_md_to_ipynb(
-    test_md_file_path=Path("./tests/test.md"),
-    test_ipynb_file_path=Path("./tests/test.ipynb"),
-) -> (None):
+# todo: add tests for exception
+def test_md_to_ipynb(get_toc_file: str, get_ipynb_file: str) -> (None):
 
     # todo: update args
-    md_to_ipynb(yaml.safe_load(test_toc))
+    md_to_ipynb(yaml.safe_load(get_toc_file))
 
     os.chdir("scripts")
 
-    # Test that test ipynb exists
-    print(os.path.exists(test_ipynb_file_path))
-
     # Test len ipynb
-    with open(test_ipynb_file_path, "r") as test_ipynb_file:
+    with open(get_ipynb_file, "r") as test_ipynb_file:
         test_ipynb_readed = test_ipynb_file.read()
     assert len(test_ipynb_readed) == 685
