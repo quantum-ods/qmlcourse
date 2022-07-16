5c0eef48f80bc528cb34916ddfb62332dd422fa1
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2022-07-16 18:22:27 +0300

    refactor

--- tests/convert_test.py
+++ tests/convert_test.py
@@ -6,10 +6,9 @@ from tools.convert import md_to_ipynb
 
 
 # todo: add tests for exception
-def test_md_to_ipynb(get_ipynb_file: str) -> (None):
+def test_md_to_ipynb(get_ipynb_file: str) -> (None):  # noqa: F811
 
     try:
-
         md_to_ipynb(dir_2_toc="./tests/data", dir_2_ipynb="./tests", testing=True)
 
         path_2_converted_lecture_to_ipynb = Path("./tests/data/lecture.ipynb")
