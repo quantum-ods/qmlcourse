54520de7befebeecec0ced3414792fe6c1fa5f5e
vtrokhymenko <vtrokhymenko@users.noreply.github.com>
2022-07-31 15:40:55 +0000

    deploy: e9a2853af673206b35be00f5f9a3b6701b2e9665

---
+++ latex/make.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+
+REM Command file for Sphinx documentation
+
+pushd %~dp0
+
+set PDFLATEX=latexmk -pdf -dvi- -ps-
+
+set "LATEXOPTS= "
+
+set XINDYOPTS=-L english -C utf8  -M sphinx.xdy
+set XINDYOPTS=%XINDYOPTS% -I xelatex
+if "%1" == "" goto all-pdf
+
+if "%1" == "all-pdf" (
+	:all-pdf
+	for %%i in (*.tex) do (
+		%PDFLATEX% %LATEXMKOPTS% %%i
+	)
+	goto end
+)
+
+if "%1" == "all-pdf-ja" (
+	goto all-pdf
+)
+
+if "%1" == "clean" (
+	del /q /s *.dvi *.log *.ind *.aux *.toc *.syn *.idx *.out *.ilg *.pla *.ps *.tar *.tar.gz *.tar.bz2 *.tar.xz *.fls *.fdb_latexmk
+	goto end
+)
+
+:end
+popd
